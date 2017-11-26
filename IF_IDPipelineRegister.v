71e8dc6f7e3ad3cacc1b1ff59668b21492654c99
unknown <dwstallings@email.arizona.edu>
2017-11-26 15:38:12 -0700

    my BAD

--- IF_IDPipelineRegister.v
+++ IF_IDPipelineRegister.v
@@ -24,11 +24,11 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-module IF_IDPipelineRegister(NewPCAddress, Instruction, Clk, outputAddress, outputInstruction, En);
+module IF_IDPipelineRegister(NewPCAddress, Instruction, Clk, outputAddress, outputInstruction, En, Flush);
 
 	input [31:0] NewPCAddress;
 	input [31:0] Instruction;
-	input Clk, En;
+	input Clk, En, Flush;
 	
 	(* dont_touch = "true" *) reg [31:0] currentInstruction, currentAddress;
 
@@ -50,22 +50,31 @@ module IF_IDPipelineRegister(NewPCAddress, Instruction, Clk, outputAddress, outp
 	
 	always@(negedge Clk)
         begin
+            
+            currentAddress <= currentAddress;
+            currentInstruction <= currentInstruction;
             if(En == 1'b1)
             begin
                 currentAddress <= NewPCAddress;
                 currentInstruction <= Instruction;
             end
-            else
+            if(Flush == 1'b1)
+            begin
+                currentAddress <= 32'd0;
+                currentInstruction <= 32'd0;
+            end
+            /*else
             begin
                 currentAddress <= currentAddress;
                 currentInstruction <= currentInstruction;
-            end
+            end*/
         end
         
         always@(posedge Clk)
         begin
             outputAddress <= currentAddress;
             outputInstruction <= currentInstruction;
+        
         end
 	
 	
--- MEM_WBPipelineRegister.v
+++ MEM_WBPipelineRegister.v
@@ -24,18 +24,18 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-module MEM_WBPipelineRegister(RegWriteIn, MemtoRegIn, ReadDataMemoryIn, ALUResultIn, DestinationRegisterIn, Clk, RegWriteOut, MemtoRegOut, ReadDataMemoryOut, ALUResultOut, DestinationRegisterOut);
+module MEM_WBPipelineRegister(RegWriteIn, MemtoRegIn, BranchIn, ReadDataMemoryIn, ALUResultIn, DestinationRegisterIn, Clk, RegWriteOut, MemtoRegOut, BranchOut, ReadDataMemoryOut, ALUResultOut, DestinationRegisterOut);
 
-	input RegWriteIn, MemtoRegIn;
+	input RegWriteIn, MemtoRegIn, BranchIn;
 	input [31:0] ALUResultIn, ReadDataMemoryIn;
 	input [4:0] DestinationRegisterIn;
 	input Clk;
 	
-	(* dont_touch = "true" *) reg CurrentRegWrite, CurrentMemtoReg;
+	(* dont_touch = "true" *) reg CurrentRegWrite, CurrentMemtoReg, CurrentBranch;
 	(* dont_touch = "true" *) reg [31:0] CurrentALUResult, CurrentReadDataMemory;
 	(* dont_touch = "true" *) reg [4:0] CurrentDestinationRegister;
 
-	output reg RegWriteOut, MemtoRegOut;
+	output reg RegWriteOut, MemtoRegOut, BranchOut;
 	output reg[31:0] ALUResultOut, ReadDataMemoryOut;
 	output reg[4:0] DestinationRegisterOut;
 
@@ -45,6 +45,7 @@ module MEM_WBPipelineRegister(RegWriteIn, MemtoRegIn, ReadDataMemoryIn, ALUResul
 	begin
 		CurrentRegWrite <= RegWriteIn;
 		CurrentMemtoReg <= MemtoRegIn;
+		CurrentBranch <= BranchIn;
 		CurrentReadDataMemory <= ReadDataMemoryIn;
 		CurrentALUResult <= ALUResultIn;
 		CurrentDestinationRegister <= DestinationRegisterIn;
@@ -54,6 +55,7 @@ module MEM_WBPipelineRegister(RegWriteIn, MemtoRegIn, ReadDataMemoryIn, ALUResul
 	begin
 		RegWriteOut <= CurrentRegWrite;
 		MemtoRegOut <= CurrentMemtoReg;
+		BranchOut <= CurrentBranch;
 		ReadDataMemoryOut <= CurrentReadDataMemory;
 		ALUResultOut <= CurrentALUResult;
 		DestinationRegisterOut <= CurrentDestinationRegister;
